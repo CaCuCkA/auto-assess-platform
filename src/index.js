70731cbcc73b616d62f517de4877fc53dc176cde
Mykola <nickolay.yakovkin@gmail.com>
2025-02-15 12:33:44 +0000

    feat: provide small changes in case of user session

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
 const express = require("express");
 const path = require("path");
 const hbs = require("hbs");
+const session = require("express-session");
 
-const authRoutes = require("./routes/auth");
-// const homeRoutes = require("./routes/home");
+require('dotenv').config({
+    override: true,
+    path: path.join(__dirname, '../.env')
+});
 
-require("./config/database");
+const authRoutes = require("./routes/authRoutes");
+const homeRoutes = require("./routes/homeRoutes");
+const homeworkRoutes = require("./routes/homeworkRoutes");
+const authMiddleware = require("./util/authMiddleware"); 
 
 const app = express();
 
 const publicPath = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates");
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+        cookie: {
+            secure: false,
+            httpOnly: true,
+            maxAge: 30 * 60 * 1000
+        }
+    })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -19,12 +38,14 @@ app.use(express.static(publicPath));
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
 
-app.use("/", authRoutes);
-// app.use("/", homeRoutes);
+app.use("/auth", authRoutes);
+app.use(authMiddleware); 
+app.use("/", homeRoutes);
+app.use("/homework", homeworkRoutes);
 
 app.use((req, res) => {
-    res.status(404).send("Page Not Found");
+    res.status(404).render("error");
 });
 
-const PORT =  3000;
+const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
