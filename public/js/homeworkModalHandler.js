cc00388fb2252dc2a28445a96426eb30a4abb297
Mykola <nickolay.yakovkin@gmail.com>
2025-03-07 21:26:52 +0000

    feat: update homework option, edit & delete modals

--- public/js/homeworkModalHandler.js
+++ public/js/homeworkModalHandler.js
@@ -1,15 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
     const MAX_TITLE_LENGTH = 255;
-    const modal = document.getElementById("create-card-window-modal");
-    const errorMessage = document.getElementById("error-message");
-    const warningMessage = document.getElementById("warning-message");
-    const groupNameInput = document.getElementById("homework-card-name");
+
+    const optionModal = document.getElementById("option-modal");
+    const closeOptionModal = document.getElementById("option-modal-close");
+    
+    const editOption = document.getElementById("edit-option");
+    const groupNameInput = document.getElementById("homework-name");
     const addHomeworkBtn = document.getElementById("add-homework-card");
-    const closeModalBtn = document.querySelector(".close");
+    const createCardWindowModal = document.getElementById("create-card-modal");
+    const closeCreateModal = createCardWindowModal.querySelector(".close");
     const submitBtn = document.getElementById("submit-btn-homework-modal");
-    const deleteModal = document.getElementById("delete-confirmation-modal");
-    const confirmDeleteBtn = document.getElementById("confirm-delete-btn");
+    
+    const notificationMessage = document.getElementById("notification-homework-message");
+
+    const deleteOption = document.getElementById("delete-option");
     const cancelDeleteBtn = document.getElementById("cancel-delete-btn");
+    const confirmDeleteBtn = document.getElementById("confirm-delete-btn");
+    const deleteConfirmationModal = document.getElementById("delete-confirmation-modal");
+    const closeDeleteModal = deleteConfirmationModal.querySelector(".close");
+
     
     const informationModal = {
         modal: document.getElementById("informational-modal"),
@@ -19,28 +28,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let currentGroupId = null;
 
-    const checkElementsExist = (elements) => elements.every(Boolean);
-
-    if (!checkElementsExist([modal, addHomeworkBtn, closeModalBtn, submitBtn, deleteModal])) {
-        console.error("❌ Missing elements! Check IDs.");
-        return;
-    }
-
     const clearMessages = () => {
-        errorMessage.textContent = "";
-        warningMessage.textContent = "";
+        groupNameInput.value = "";
+        notificationMessage.value = "";
     };
 
-    const showModal = () => {
+    const closeModal = (modal) => {
         clearMessages();
-        modal.style.display = "flex";
-        groupNameInput.value = ""; 
-    };
-
-    const closeModal = () => {
         modal.style.display = "none";
-        submitBtn.textContent = "Create";
-        currentGroupId = null;
     };
 
     const displayNotification = (type, notTitle, message) => {
@@ -51,30 +46,68 @@ document.addEventListener("DOMContentLoaded", () => {
         modal.style.display = "flex";
     };
 
+    const displayNotificationMessage = (message, type="error") => {
+        notificationMessage.style.color = type === "error" ? "#FF6347" : "#FFAA1D";
+        notificationMessage.textContent = message;
+    };
+
     const isGroupNameTaken = async (groupName) => {
         const response = await fetch(`/check-homework-name/${groupName}`);
         const { exists } = await response.json();
         return exists;
     };
 
-    const handleSubmit = async () => {
-        const groupName = groupNameInput.value.trim();
+    // option modal menu
+
+    document.querySelectorAll(".homework-card-edit-icon").forEach(editIcon => {
+        editIcon.addEventListener("click", (event) => {
+            event.preventDefault();
+            event.stopPropagation();
+            const cardContainer = event.target.closest(".homework-card-container");
+            currentGroupId = cardContainer.getAttribute("data-id");
+            optionModal.style.display = "flex";
+        });
+    });  
+    
+    closeOptionModal.addEventListener("click", () => {closeModal(optionModal)});
+
+
+    // add/edit homework and their submit button 
+
+    addHomeworkBtn.addEventListener("click", () => {
+        clearMessages();
+        currentGroupId = null;
+        submitBtn.textContent = "Create";
+        createCardWindowModal.style.display = "flex";
+    });  
+
+    editOption.addEventListener("click", () => {
+        closeModal(optionModal);
+        submitBtn.textContent = "Edit";
+        createCardWindowModal.style.display = "flex";
+    });
+
+    closeCreateModal.addEventListener("click", () => {closeModal(createCardWindowModal)});
+
+    submitBtn.addEventListener("click", async () => {
+        const groupName = groupNameInput.value;
         clearMessages();
 
         if (!groupName) {
-            warningMessage.textContent = "Please enter group name";
+            displayNotificationMessage("Please enter group name", "warning");
             return;
         }
 
         if (await isGroupNameTaken(groupName)) {
-            errorMessage.textContent = "Group with this name already exists!";
+            displayNotificationMessage("Group with this name already exists!");
             return;
         }
 
+
         if (groupName.length > MAX_TITLE_LENGTH) {
-            errorMessage.textContent = `The group name is too long. Please keep it under ${MAX_TITLE_LENGTH} characters.`;
+            displayNotificationMessage(`The group name is too long. Please keep it under ${MAX_TITLE_LENGTH} characters.`);
             return;
-        }        
+        }  
 
         const url = currentGroupId ? `/edit-homework/${currentGroupId}` : "/add-homework"; 
         const method = currentGroupId ? "PUT" : "POST";
@@ -85,116 +118,55 @@ document.addEventListener("DOMContentLoaded", () => {
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ title: groupName })
             });
-            console.log(response);
-            const { success, error } = await response.json();
-            if (success) {
-                clearMessages();
-                closeModal();
-                window.location.reload();
-            } else {
-                handleError(error);
-            }
+            await response.json();
+            closeModal(createCardWindowModal);
+            window.location.reload();
         } catch (error) {
             console.error("❌ Fetch error:", error);
-            errorMessage.textContent = "Oops, something went wrong. Try again!";
+            displayNotificationMessage("Oops, something went wrong. Try again!");
         }
-    };
-
-    const handleError = (error) => {
-        if (error === "exists") {
-            errorMessage.textContent = "Group with this name already exists!";
-        } else {
-            errorMessage.textContent = "Oops, something went wrong. Try again!";
-        }
-    };
-
-    addHomeworkBtn.addEventListener("click", showModal);
-    closeModalBtn.addEventListener("click", closeModal);
-    submitBtn.addEventListener("click", handleSubmit);
-
-    const handleEditClick = (event) => {
-        event.preventDefault();
-        const cardContainer = event.target.closest(".homework-card-container");
-        currentGroupId = cardContainer.getAttribute("data-id"); 
-        const groupName = cardContainer.querySelector(".homework-card-title").textContent.trim();
-
-        groupNameInput.value = groupName;
-        submitBtn.textContent = "Rename";
-        console.log("[handleEditClick] Editing group with ID:", currentGroupId, "and name:", groupName);
-        showModal();
-    };
-
-    document.querySelectorAll(".edit-option").forEach(editOption => {
-        editOption.addEventListener("click", handleEditClick);
     });
+    
 
-    const handleDeleteClick = (event) => {
+    // delete modal 
+
+    deleteOption.addEventListener("click", () => {
+        closeModal(optionModal);
+        deleteConfirmationModal.style.display = "flex";
+    });
+    
+    closeDeleteModal.addEventListener("click", () => {closeModal(deleteConfirmationModal)});
+
+    cancelDeleteBtn.addEventListener("click", () => {closeModal(deleteConfirmationModal)});
+
+    confirmDeleteBtn.addEventListener("click", async (event) => {
         event.preventDefault();
-        const cardContainer = event.target.closest(".homework-card-container");
-        currentGroupId = cardContainer.getAttribute("data-id");
-        console.log("[handleDeleteClick] Deleting group with ID:", currentGroupId);
+        closeModal(deleteConfirmationModal);
 
-        deleteModal.style.display = "flex";
+        try {
+            const response = await fetch(`/delete-homework/${currentGroupId}`, {
+                method: "DELETE",
+            });
+            const data = await response.json();
 
-        confirmDeleteBtn.addEventListener("click", async () => {
-            console.log("[handleDeleteClick] User confirmed delete");
-
-            try {
-                const response = await fetch(`/delete-homework/${currentGroupId + 100}`, {
-                    method: "DELETE",
-                });
-                const data = await response.json();
-
-                if (data.success) {
-                    console.log("[handleDeleteClick] Group deleted successfully.");
-                    window.location.reload();
-                } else {
-                    console.error("[handleDeleteClick] Error deleting group:", data.error);
-                    displayNotification("error", "Error deleting homework!", "There was an issue while attempting to delete the homework. Please try again later.");
-                }
-            } catch (error) {
-                console.error("[handleDeleteClick] Error during fetch request:", error);
+            if (data.success) {
+                console.log("[handleDeleteClick] Group was deleted successfully.");
+                window.location.reload();
+            } else {
+                console.error("[handleDeleteClick] Error deleting group:", data.error);
+                displayNotification("error", "Error deleting homework!", "There was an issue while attempting to delete the homework. Please try again later.");
             }
-
-            deleteModal.style.display = "none";
-        });
-
-        cancelDeleteBtn.addEventListener("click", () => {
-            deleteModal.style.display = "none";
-        });
-    };
-
-    document.querySelectorAll(".delete-option").forEach(deleteOption => {
-        deleteOption.addEventListener("click", handleDeleteClick);
+        } catch (error) {
+            console.error("[handleDeleteClick] Error during fetch request:", error);
+        }
     });
 
-    document.querySelectorAll(".homework-card-edit-icon").forEach(editIcon => {
-        editIcon.addEventListener("click", (event) => {
-            event.preventDefault();
-            event.stopPropagation();
-            
-            const cardContainer = event.target.closest(".homework-card-container");
-            const dropdownMenu = cardContainer.querySelector(".dropdown-menu");
-            dropdownMenu.style.display = "block"; 
-
-            const closeMenu = () => {
-                dropdownMenu.style.display = "none";
-                document.removeEventListener("click", closeMenu);
-            };
-
-            document.addEventListener("click", closeMenu);
-        });
-    });
-
-    window.addEventListener("click", (event) => {
-        if (event.target === modal) closeModal();
-    });
-
-    document.querySelector(".close-informational-modal").addEventListener("click", function () {
-        informationModal.modal.style.display = "none";
+    
+    document.getElementById("close-informational-modal").addEventListener("click", function () {
+        closeModal(informationModal);
     });
 
     document.getElementById("close-informational-modal-btn").addEventListener("click", function () {
-        informationModal.modal.style.display = "none";
+        closeModal(informationModal);
     });
 });
