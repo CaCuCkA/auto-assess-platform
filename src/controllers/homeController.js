bc539cc7e87860d78874e97db34ed7404dae2581
Mykola <nickolay.yakovkin@gmail.com>
2025-03-22 17:56:25 +0000

    feat: add unreviewed reports on main page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,15 +1,24 @@
+const Report = require("../models/Report");
 const Homework = require("../models/Homework");
 const { getRandomCardColor } = require("../util/colorGenerator")
 
 exports.renderHome = async (req, res) => {
     try {
+        const reportData = await Report.getUnreviewed();
         const homeworkData = await Homework.getAllByAdmin(req.session.userId);
         const homeworkWithUrls = homeworkData.map(homework => ({
             ...homework,
             url: `/homework/${homework.homework_id}`
         }));
-        
-        res.render("home", { homeworkWithUrls });
+
+        const reportWithUrls = reportData.map(report => ({
+                ...report,
+                url: `/report/report-editor/${report.report_id}?id=${report.participant_id}&is_home_page=true`,
+                "status": report.is_submited ? "submitted" :
+                          report.updated_at !== report.created_at ? "reviewed" :
+                          "unreviewed"
+        }));
+        res.render("home", { homeworkWithUrls, reports: reportWithUrls});
     } catch (error) {
         console.error("Error fetching homework:", error);
         res.status(500).json({ error: "Internal Server Error" }); 
