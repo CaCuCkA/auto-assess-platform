6ff8e16da660d484bf479fbc202bbec6f14220d7
Mykola <nickolay.yakovkin@gmail.com>
2025-03-23 22:40:49 +0000

    chore: add logging

---
+++ backend/utils/logging.py
@@ -0,0 +1,15 @@
+import logging
+import betterlogging as bl
+
+def setup_logging():
+    log_level = logging.INFO
+    bl.basic_colorized_config(level=log_level)
+
+    logging.basicConfig(
+        level=log_level,
+        format="%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s",
+    )
+    logging.getLogger(__name__).info("Logging is configured.")
+
+def get_logger(name: str = None):
+    return logging.getLogger(name)
