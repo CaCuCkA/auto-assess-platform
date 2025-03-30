0f1d03444fcec67bcfde36e5d862cb59f3787c90
Mykola Yakovkin <nickolay.yakovkin@gmail.com>
2025-03-30 22:02:35 +0000

    make port as env parameter

--- backend/main.py
+++ backend/main.py
@@ -6,8 +6,10 @@ from config import load_config
 from utils import setup_logging, get_logger
 from database import create_engine, create_session_pool, DatabaseGateway
 
-
 app = Quart(__name__)
+
+config = load_config("../.env")
+app.config["CONFIG"] = config
 
 app.register_blueprint(jenkins_bp, url_prefix='/jenkins')
 app.register_blueprint(github_bp, url_prefix='/github')
@@ -17,16 +19,13 @@ logger = get_logger(__name__)
 @app.before_serving
 async def init_resources():
     setup_logging()
-    config = load_config("../.env")
-    app.config["CONFIG"] = config
     
     logger.info("Initializing DB resources...")
-    engine = create_engine(config.db)
+    engine = create_engine(app.config["CONFIG"].db)
     session_pool = await create_session_pool(engine)
     app.config["SESSION_POOL"] = session_pool
 
     logger.info("DB session pool created successfully.")
-
 
 @app.before_request
 async def init_db_gateway():
@@ -34,18 +33,16 @@ async def init_db_gateway():
     session = session_pool()
     g.db_gateway = DatabaseGateway(session)
 
-
 @app.after_request
 async def cleanup_db_gateway(response):
     if hasattr(g, "db_session"):
         await g.db_session.close()
     return response
 
-
 @app.route("/")
 async def index():
     return {"message": "Quart app is running with DB Gateway!"}
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=5000)
+    port = app.config["CONFIG"].backend.port
+    app.run(host="0.0.0.0", debug=True, port=port)
