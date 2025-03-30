1b9a15cf93ee8882a490684dc3bb225184e13aac
Mykola Yakovkin <nickolay.yakovkin@gmail.com>
2025-03-30 22:01:16 +0000

    chore: minor changes

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,5 @@
 import json
-from dataclasses import dataclass, asdict
-
+from dataclasses import dataclass, asdict, field
 from environs import Env
 from sqlalchemy.engine.url import URL
 
@@ -17,7 +16,7 @@ class PullRequestPayload:
 
     def to_json(self):
         return json.dumps(asdict(self))
-    
+
 
 @dataclass
 class DbConfig:
@@ -27,29 +26,16 @@ class DbConfig:
     host: str
     port: int
 
-    def construct_sqlalchemy_url(self, driver="asyncpg", host=None, port=None) -> str:
-        if not host:
-            host = self.host
-        if not port:
-            port = self.port
+    def construct_sqlalchemy_url(self, driver="asyncpg") -> str:
         uri = URL.create(
             drivername=f"postgresql+{driver}",
             username=self.user,
             password=self.password,
-            host=host,
-            port=port,
+            host=self.host,
+            port=self.port,
             database=self.database,
         )
         return uri.render_as_string(hide_password=False)
-
-    @staticmethod
-    def from_env(env: Env):
-        user = env.str("POSTGRES_USER")
-        password = env.str("POSTGRES_PASSWORD")
-        database = env.str("POSTGRES_DB")
-        host = env.str("DB_HOST")
-        port = env.int("DB_PORT")
-        return DbConfig(user=user, password=password, database=database, host=host, port=port)
 
 
 @dataclass
@@ -58,38 +44,54 @@ class Jenkins:
     user: str
     token: str
 
-    @staticmethod
-    def from_env(env: Env):
-        url = env.str("JENKINS_URL")
-        user = env.str("JENKINS_USER")
-        token = env.str("JENKINS_API_TOKEN")
-        return Jenkins(url=url, user=user, token=token)
-
 
 @dataclass
 class Gemini:
     token: str
     model: str
 
-    @staticmethod
-    def from_env(env: Env):
-        token = env.str("GEMINI_API_TOKEN")
-        model = env.str("GEMINI_MODEL")
-        return Gemini(token=token, model=model) 
-    
+
+@dataclass
+class Backend:
+    port: int
+
 
 @dataclass
 class Config:
     db: DbConfig
     jenkins: Jenkins
     gemini: Gemini
+    backend: Backend
+    env: Env = field(default_factory=Env)
+
+    @classmethod
+    def from_env(cls, path: str = None):
+        env = Env()
+        env.read_env(path)
+
+        return cls(
+            db=DbConfig(
+                user=env.str("POSTGRES_USER"),
+                password=env.str("POSTGRES_PASSWORD"),
+                database=env.str("POSTGRES_DB"),
+                host=env.str("DB_HOST"),
+                port=env.int("DB_PORT")
+            ),
+            jenkins=Jenkins(
+                url=env.str("JENKINS_URL"),
+                user=env.str("JENKINS_USER"),
+                token=env.str("JENKINS_API_TOKEN")
+            ),
+            gemini=Gemini(
+                token=env.str("GEMINI_API_TOKEN"),
+                model=env.str("GEMINI_MODEL")
+            ),
+            backend=Backend(
+                port=env.int("BACKEND_PORT")
+            ),
+            env=env
+        )
+
 
 def load_config(path: str = None) -> Config:
-    env = Env()
-    env.read_env(path)
-
-    return Config(
-        db=DbConfig.from_env(env),
-        jenkins=Jenkins.from_env(env),
-        gemini=Gemini.from_env(env),
-    )
+    return Config.from_env(path)
